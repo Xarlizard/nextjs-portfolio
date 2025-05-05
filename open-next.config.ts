717c31ce39c6bc9fc05aa4fffe2bdba9e1ce51f9
xarlizard <charkrios@gmail.com>
2025-05-06 01:07:07 +0200

    remove opennext.config from gitignore

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  incrementalCache: undefined, // Use undefined if you are not using R2 for caching
+});
