cb484aa71a07e0b11a6e38e21e7c0b34b2bf6282
xarlizard <charkrios@gmail.com>
2025-05-05 20:51:43 +0200

    upload Resume

---
+++ components/about.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        A seasoned{" "}
+        <span className="font-medium">Senior React Fullstack Developer</span>{" "}
+        with a strong frontend focus and comprehensive backend capabilities.
+        Extensive experience in building sophisticated web applications using
+        the React ecosystem, including proficiency in{" "}
+        <span className="font-medium">React 19.0</span>,{" "}
+        <span className="font-medium">React Router v7</span>, and modern build
+        tools like <span className="font-medium">Nextjs/Vite/Apollo</span>. Expertise in
+        developing and consuming{" "}
+        <span className="font-medium">RESTful APIs</span> and implementing{" "}
+        <span className="font-medium">Server-Side Rendering (SSR)</span> for
+        enhanced performance and SEO. Proven ability to collaborate effectively
+        within cross-functional teams, apply modern web development practices,
+        and ensure adherence to accessibility guidelines.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,122 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact, FaNodeJs, FaPython, FaCloud } from "react-icons/fa";
+import { SiNextdotjs, SiJavascript, SiMongodb } from "react-icons/si";
+import circleImg from "@/public/circleImg.png";
+import codeticketsImg from "@/public/codeticketsImg.png";
+import tldrawImg from "@/public/tldrawImg.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+export const experiencesData = [
+  {
+    title: "FRONTEND WEB DEVELOPER (REACT)",
+    location: "Mataro (remote), Spain",
+    description:
+      "Currently driving frontend development at APLICACIONES WEB MARDO, S.L. in charge of the solo-team at Codetickets, utilizing React 19.0 and related technologies to build robust and scalable applications. Proficient in integrating with custom APIs built on RESTful/CRUD principles and implementing Server-Side Rendering (SSR) with the frameworks ReactRouter v7 (Remix).",
+    icon: React.createElement(FaReact),
+    date: "20/11/2024 - CURRENT",
+  },
+  {
+    title: "FULLSTACK WEB DEVELOPER (Freelance / PIXELIMPERIUM)",
+    location: "Ibiza (remote), Spain",
+    description:
+      "Successfully managed the full lifecycle of eCommerce projects, leveraging a diverse tech stack including React and Next.js for the frontend and Node.js, Python, and Java for backend development. Experienced in deploying and managing applications on various cloud platforms (AWS, Cloudflare Pages, Vercel, Netlify).",
+    icon: React.createElement(SiNextdotjs),
+    date: "12/2022 - 09/2024",
+  },
+  {
+    title: "IT CONSULTANT & APP(S) DEVELOPER",
+    location: "Molins De Rei, Spain",
+    description:
+      "Contributed to the development of finance-focused applications using React & React Native on the frontend and NodeJS and Java on the backend, deployed on AWS and Oracle Cloud Servers. Took a leadership role in training junior developers.",
+    icon: React.createElement(FaReact),
+    date: "05/2022 - 09/2022",
+  },
+  {
+    title: "IT CONSULTANT & WEB DEVELOPER JUNIOR",
+    location: "Hospitalet de Llobregat, Spain",
+    description:
+      "Oversaw and co-managed the IT department and a team of technicians and developers, team in charge of developing and maintaining web applications using technologies such as React + Node.js, PHP (Wordpress), and Angular+Java.",
+    icon: React.createElement(FaNodeJs),
+    date: "05/2020 - 05/2022",
+  },
+  {
+    title: "SYSTEM ADMIN (L2) & JUNIOR FRONTEND DEVELOPER",
+    location: "Cornellà, Spain",
+    description:
+      "Gained initial experience with React development while managing IT systems and networks for B2B clients.",
+    icon: React.createElement(FaCloud),
+    date: "05/2017 - 05/2020",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Codetickets",
+    description:
+      "Ticketing Saas as vendor portal dashboard/retail for some public and private institutions, managing and serving their event tickets.",
+    tags: ["React", "Node.js", "Cloudflare R2 Database", "Bootstrap", "AWS"],
+    imageUrl: codeticketsImg,
+  },
+  {
+    title: "TLDRAW with Nextjs",
+    description:
+      "Tldraw editor made with React and shadcdn, integrating AI with OpenAI API to generate custom SVG forms.",
+    tags: ["tRPC", "React", "TypeScript", "Next.js", "Tailwind", "Shadcdn"],
+    imageUrl: tldrawImg,
+  },
+  {
+    title: "Circle",
+    description:
+      "A simple project landing page featuring a modern UI layout, responsive and designed for Ironhack's Bootcamp.",
+    tags: ["React", "CSS Modules", "ReactRouter", "Bootstrap", "Axios"],
+    imageUrl: circleImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Shadcdn",
+  "Framer Motion",
+] as const;
