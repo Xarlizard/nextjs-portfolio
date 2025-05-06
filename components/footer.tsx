7618f484ea22e7cf658f148f325da3e1e9aa4794
xarlizard <charkrios@gmail.com>
2025-05-06 14:55:52 +0200

    update Footer Service Provider mention

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Footer() {
+  const [hostingProvider, setHostingProvider] = useState("Github");
+
+  useEffect(() => {
+    const hostname = window.location.hostname;
+
+    if (
+      hostname.includes("charlie.icu") ||
+      hostname.includes("pages.dev") ||
+      hostname.includes("workers.dev")
+    ) {
+      setHostingProvider("Cloudflare");
+    } else if (hostname.includes("netlify.app")) {
+      setHostingProvider("Netlify");
+    } else if (hostname.includes("vercel.app")) {
+      setHostingProvider("Vercel");
+    } else {
+      setHostingProvider("Github");
+    }
+  }, []);
+
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
@@ -9,7 +29,7 @@ export default function Footer() {
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
         React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        Framer Motion, React Email & Resend, {hostingProvider} hosting.
       </p>
     </footer>
   );
