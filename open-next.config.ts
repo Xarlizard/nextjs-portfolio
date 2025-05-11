58860988555b4fad9b17d1dea22175f8f29da787
xarlizard <charkrios@gmail.com>
2025-05-11 20:25:59 +0200

    fix name

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,9 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+let defineCloudflareConfig;
+
+if (process.env.CLOUDFLARE) {
+  defineCloudflareConfig = (await import("@opennextjs/cloudflare")).defineCloudflareConfig;
+}
 
 export default defineCloudflareConfig({
   incrementalCache: undefined, // Use undefined if you are not using R2 for caching
