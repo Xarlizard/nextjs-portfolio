d222aa6c8737a3dc72b2878fad591a008667de4e
xarlizard <charkrios@gmail.com>
2025-05-11 20:30:45 +0200

    Revert "fix name"
    This reverts commit 58860988555b4fad9b17d1dea22175f8f29da787.

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,5 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
-let defineCloudflareConfig;
-
-if (process.env.CLOUDFLARE) {
-  defineCloudflareConfig = (await import("@opennextjs/cloudflare")).defineCloudflareConfig;
-}
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
   incrementalCache: undefined, // Use undefined if you are not using R2 for caching
