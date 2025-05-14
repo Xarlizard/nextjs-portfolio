87274be0ba677aed2b372eb7fcdb54903d089e40
xarlizard <charkrios@gmail.com>
2025-05-14 18:54:09 +0200

    undo latest update

--- components/project.tsx
+++ components/project.tsx
@@ -12,7 +12,6 @@ export default function Project({
   description,
   tags,
   imageUrl,
-  url,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
@@ -23,48 +22,48 @@ export default function Project({
   const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
   return (
-    <a
-      href={url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="block"
+    <motion.div
+      ref={ref}
+      style={{
+        scale: scaleProgess,
+        opacity: opacityProgess,
+      }}
+      className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <motion.div
-        ref={ref}
-        style={{
-          scale: scaleProgess,
-          opacity: opacityProgess,
-        }}
-        className="group mb-3 sm:mb-8 last:mb-0"
-      >
-        <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-          <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-            <h3 className="text-2xl font-semibold">{title}</h3>
-            <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
-              {description}
-            </p>
-            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-              {tags.map((tag, index) => (
-                <li
-                  className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                  key={index}
-                >
-                  {tag}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="absolute top-0 left-0 w-full h-full">
-            <Image
-              src={imageUrl}
-              alt={title}
-              layout="fill"
-              objectFit="cover"
-              className="opacity-50 sm:opacity-100"
-            />
-          </div>
-        </section>
-      </motion.div>
-    </a>
+      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+            {description}
+          </p>
+          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+            {tags.map((tag, index) => (
+              <li
+                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                key={index}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <Image
+          src={imageUrl}
+          alt="Project I worked on"
+          quality={95}
+          className="absolute hidden sm:block top-4 sm:top-1/2 sm:-translate-y-1/2 sm:right-4 sm:max-w-[18rem] sm:max-h-[14rem] sm:min-h-full object-contain rounded-lg shadow-2xl
+            transition 
+            group-hover:scale-[2] /* Extreme zoom effect */
+            group-hover:rotate-1 /* Slight tilt effect */
+            group-hover:translate-x-4 /* Move image to the right on hover */
+
+            group-even:sm:left-4 /* Position image on the left for even groups */
+            group-even:sm:right-[initial] /* Reset right positioning for even groups */
+            group-even:group-hover:translate-x-[-4rem] /* Move image to the left on hover for even groups */
+            group-even:group-hover:rotate-1"
+        />
+      </section>
+    </motion.div>
   );
 }
